b85c8bbe83b05e67c55bf3660b993ea23836a631
Dibakar Dey <110836157+dibakar63@users.noreply.github.com>
2023-07-17 18:45:50 +0530

    Update orderActions.js

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -5,7 +5,7 @@ export const placeOrder=(token,subTotal)=>async(dispatch,getState)=>{
         const currentUser=getState().loginUserReducer.currentUser
         const cartItems=getState().cartReducer.cartItems
      
-        const response=await axios.post('/api/orders/placeorder',{token,subTotal,currentUser,cartItems})
+        const response=await axios.post('https://pizza-02ri.onrender.com/api/orders/placeorder',{token,subTotal,currentUser,cartItems})
         dispatch({type:"PLACE_ORDER_SUCCESS"})
         console.log(response)
 
